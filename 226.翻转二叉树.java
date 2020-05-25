2b33485cd992a9baf9ef0c950032a5bbdbb2b8cb
xp <realxp141414@163.com>
2020-05-25 20:54:40 +0800

    xx

---
+++ 226.翻转二叉树.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+
+    }
+}
+// @lc code=end
+
