0cbc08063e4c380b05cd9513269b29e2c6eec78f
xp664221812 <realxp141414@163.com>
2020-08-20 18:20:05 +0800

    add

--- 8.字符串转换整数-atoi.java
+++ 8.字符串转换整数-atoi.java
@@ -7,17 +7,33 @@
 // @lc code=start
 class Solution {
     public int myAtoi(String str) {
-        int sign = 1, total = 0;
+        int sign = 1, total = 0, index = 0;
         str = str.trim();
 
-        char[]ch=str.toCharArray();
-        // for (int i = 0; i < ch.length; i++) {
-        //     if(ch[i]=='-'){
+        char[] ch = str.toCharArray();
 
-        //     }
+        if (str.charAt(index) == '-') {
+            sign = -1;
+            index++;
+        } else if (str.charAt(index) == '+') {
+            index++;
+        } else {
+            return 0;
+        }
+
+        while(index<str.length()){
             
+        }
+
+
+
+
+        // for (int i = 0; i < ch.length; i++) {
+        // if(ch[i]=='-'){
+
         // }
 
+        // }
 
     }
 }
