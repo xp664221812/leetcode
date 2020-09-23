1ec452e5a1d84aa660b3ee27059ae5c30666145a
xp664221812 <realxp141414@163.com>
2020-09-23 17:01:33 +0800

    add

--- 344.反转字符串.java
+++ 344.反转字符串.java
@@ -14,13 +14,19 @@ class Solution {
         }
     }
 
-
-
     public void reverseString1(char[] s) {
-        for (int i = 0,j=s.length-1; i < j; i++,j--) {
-            char temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
+
+    public void reverseString2(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
         }
     }
 
--- _21_Merge2list.java
+++ _21_Merge2list.java
@@ -117,21 +117,21 @@ class Solution {
 
         while (t1 != null && t2 != null) {
             if (t1.val < t2.val) {
-                current.next=t1;
-                current=t1;
-                t1=t1.next;
-            }else{
-                current.next=t2;
-                current=t2;
-                t2=t2.next;
+                current.next = t1;
+                current = t1;
+                t1 = t1.next;
+            } else {
+                current.next = t2;
+                current = t2;
+                t2 = t2.next;
             }
         }
-        if(t1==null){
-            current.next=t2;
+        if (t1 == null) {
+            current.next = t2;
         }
 
-        if(t2==null){
-            current.next=t1;
+        if (t2 == null) {
+            current.next = t1;
         }
         return head.next;
 
@@ -141,6 +141,24 @@ class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         for (int i = 0; i < lists.length; i++) {
 
+        }
+    }
+
+    public ListNode mergeTwoLists6(ListNode t1, ListNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+
+        if (t2 == null) {
+            return t1;
+        }
+
+        if (t1.val < t2.val) {
+            t1.next = mergeTwoLists6(t1.next, t2);
+            return t1;
+        } else {
+            t2.next = mergeTwoLists6(t1, t2.next);
+            return t2;
         }
     }
 
