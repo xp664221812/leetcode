0337d9e30f8cfafaf29aecfb3db9b3e76d6125a8
xp664221812 <realxp141414@163.com>
2020-11-30 18:27:37 +0800

    add

--- 33.搜索旋转排序数组.java
+++ 33.搜索旋转排序数组.java
@@ -10,14 +10,45 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        int left = 0;
-        int right = nums.length;
-        while (left < right) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if (nums[mid] > nums[left] && nums[left] <= target && target < nums[right]) {
-                
+
+            if (nums[mid] == target) {
+                return mid;
+
+            } else if (nums[0] <= nums[mid]) {
+                // 如果左边是单调递增的
+                if (nums[0] <= target && target < nums[mid]) {
+                    // 目标在左边
+                    right = mid - 1;
+                } else {
+                    // 目标在右边边
+                    left = mid + 1;
+                }
+
+            } else {
+                // 如果左边不是单调递增的，那右边肯定是单调递增的
+                if (nums[mid] < target && target <= nums[nums.length - 1]) {
+                    // 目标在右边
+                    left = mid + 1;
+                } else {
+                    // 目标在左边
+                    right = mid - 1;
+                }
+
             }
         }
+
+        return -1;
     }
 }
 // @lc code=end
